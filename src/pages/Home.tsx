a7af5f96c72cfc4d9a3e1cff0c67c6b220246fde
Omeremily <omeremily1@gmail.com>
2024-03-04 13:38:17 +0200

    6

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,7 @@ export default function Home() {
       <NavBar/>
       <h1>TODO:HOME - hero section</h1>
       <SaleItemSection/>
+      <img className="img-fluid" style={{padding: "150px 25px", margin: "0 auto"}} src="./public/imgs/sideToside.jpg" alt="" />
       <Contact/>
     </>
   )
