9311ed56402ee3662c979eb72cb727381ad74503
Gal machlev <galmachlev@gmail.com>
2024-03-26 17:27:48 +0200

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,7 +16,6 @@ export default function NavBar() {
       </NavLink>
       <Container>
         <Nav className="me-auto">
-          <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
           <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
           <Nav.Link to="/register" as={NavLink}>Register</Nav.Link>
           <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
