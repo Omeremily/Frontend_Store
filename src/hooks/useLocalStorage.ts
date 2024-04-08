df30f93ec952d47ca02c2b4a87c48d20407eaa3c
Gal machlev <galmachlev@gmail.com>
2024-04-08 13:12:02 +0300

    עדכון דף לוקאל סטוראג הוק

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react";
 
 export function useLocalStorage<T>(key:string, intialValue: T | (() => T)){
     const[value, setValue] = useState<T>(() => {
+        // בדיקה האם יש ערך במפתח הנתון בזיכרון המקומי
         const jsonValue = localStorage.getItem(key)
 
+        // אם יש ערך במפתח, החזרת הערך שנמצא כערך בהתאם מסוג ג'ייסון
         if(jsonValue != null) return JSON.parse(jsonValue)
 
+        // אם אין ערך קיים במפתח, החזרת הערך הראשוני
         if(typeof intialValue === 'function'){
             return (intialValue as () => T)()
         }else{
@@ -13,12 +16,11 @@ export function useLocalStorage<T>(key:string, intialValue: T | (() => T)){
         }
     })
 
+    // שימוש ביוז-אפקט כדי לעדכן את הערך בזיכרון המקומי בכל פעם שהערך משתנה
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(value))
     }, [key, value])
 
+    // החזרת הערך הנוכחי ופונקציה לעדכון הערך
     return [value, setValue] as [typeof value, typeof setValue];
 }
-
-
-
