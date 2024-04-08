b8ff5cb07a68e1dcd21fbfbd7c354d4ada102624
Gal machlev <galmachlev@gmail.com>
2024-04-08 12:15:05 +0300

    הערות בעברית utilities + mian.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { ShoppingCartProvider } from './context/ShoppingCartContext'
 import ItemContextProvider from './context/ItemContext'
 
-
+// בכל טעינת של האפליקציה יש לרנדר ולבצע את הפעולות הבאות
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ItemContextProvider>
-    <UsersContextProvider>
-      <ShoppingCartProvider>
-        <RouterProvider router={routes} />
-      </ShoppingCartProvider>
-    </UsersContextProvider>
-    </ItemContextProvider>
 
+  /* יצירת מצב שבעזרתו ניתן לזהות ולתקן בעיות פוטנציאליות באפליקציה
+     מאפשר התראה על פונקציות מיושנות ושימוש בפעולות שלא מאוד מתקדמות
+     ומסייע לכתוב אפליקציה ריאקט יותר נקייה ומתקדמת */
+  <React.StrictMode>
+    {/* ספק מצב משתמש המספק את נתוני המשתמש הרלוונטיים לכל הרכיבים בתוך האפליקציה */}
+    <ItemContextProvider>
+      {/* ספק מצב לעגלת הקניות המנהל את נתוני המוצרים בעגלת הקניות של המשתמש */}
+      <UsersContextProvider>
+        {/* ספק הניווט מאפשר ניווט באפליקציה באמצעות הגדרת נתיבים וקומפוננטות שונות עבור כל נתיב */}
+        <ShoppingCartProvider>
+          {/* בקרת הניווט מועברת לפי הנתיבים שמוגדרים בקובץ ראוטס  */}
+          <RouterProvider router={routes} />
+        </ShoppingCartProvider>
+      </UsersContextProvider>
+    </ItemContextProvider>
   </React.StrictMode>,
+
 )
