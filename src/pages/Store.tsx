d45a46c17d5552672aa9db4cf302a5cfe3067a51
Gal machlev <galmachlev@gmail.com>
2024-04-08 12:38:54 +0300

    הערות בעברית store.tsx

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -10,28 +10,37 @@ import '../css/Video.css';
 import Footer from "../components/Footer";
 import VideoHero from "../components/VideoHero";
 
+//פונקציות בדף חנות
 export default function Store() {
+    // מערך מוצרים בדף החנות שימוש ביוז-קונטקסט
     const { items } = useContext<any>(ItemContext);
+    // 
     const [itemsToShow, setItemsToShow] = useState<StoreItemProps[]>([]);
     const [search, setSearch] = useState('');
     const [sortOrder, setSortOrder] = useState('');
     const [categoryFilter, setCategoryFilter] = useState('');
 
-    {/* פונקצית מיון לפי מחיר - asc/desc */}
+    {/* פונקצית מיון לפי מחיר - asc/desc 
+    תפעל כל פעם מחדש לפי מצב המיון הנתון*/}
     useEffect(() => {
-      let sortedItems = [...items];
-      if (sortOrder === 'asc') {
-          sortedItems.sort((a, b) => (a.price ?? 0) - (b.price ?? 0));
-      } else if (sortOrder === 'desc') {
-          sortedItems.sort((a, b) => (b.price ?? 0) - (a.price ?? 0));
-      }
-      setItemsToShow(sortedItems);
+        let sortedItems = [...items];
+        // אם נבחרה אופציה זו יש למיין מנמוך לגבוה
+        if (sortOrder === 'asc') {
+            sortedItems.sort((a, b) => (a.price ?? 0) - (b.price ?? 0));
+        // אם נבחרה אופציה זו יש למיין מגבוה לנמוך
+        } else if (sortOrder === 'desc') {
+            sortedItems.sort((a, b) => (b.price ?? 0) - (a.price ?? 0));
+        }   
+        setItemsToShow(sortedItems);
     }, [sortOrder]);
   
-    {/* פונקצית סינון קטגוריית מוצר + חיפוש בדף בהתאם לסינון */}
+    {/* פונקצית סינון קטגוריית מוצר + חיפוש בדף בהתאם לסינון 
+        תפעל כל פעם מחדש לפי מצב הסינון הנתון*/}
     useEffect(() => {
+        // אם לא הוקלד טקסט בחיפוש ואין פילטור קטגוריות תגדיר מצב התחלתי
         if (search === '' && !categoryFilter) {
             setItemsToShow(items);
+        // הפעלת פלטור לפי שם המוצר או קטגוריית מוצר
         } else {
             let filteredItems = items.filter((item: StoreItemProps) => {
                 const nameMatches = item.name?.toLowerCase().includes(search);
@@ -45,11 +54,17 @@ export default function Store() {
 
     return (
         <>
+            {/* נאב-בר */}
             <NavBar />
+
+            {/* וידאו סקשיין */}
             <div id="hero">
                 <VideoHero src={"../../public/imgs/Video.mp4"} />
             </div>
+
             <Row className="mx-auto mt-3">
+
+                {/* שדה חיפוש */}
                 <Col xs={12} md={6} className="mb-3">
                     <div className="d-flex align-items-center">
                         <input
@@ -61,6 +76,8 @@ export default function Store() {
                         />
                     </div>
                 </Col>
+
+                {/* שדה מיון לפי מחיר */}
                 <Col xs={12} md={3} className="mb-3">
                     <select 
                         className="form-select"
@@ -72,6 +89,8 @@ export default function Store() {
                         <option value="desc">Sort By: High to Low</option>
                     </select>
                 </Col>
+
+                {/* שדה סינון לפי קטגוריית מוצר */}
                 <Col xs={12} md={3} className="mb-3">
                     <select 
                         className="form-select"
@@ -86,7 +105,10 @@ export default function Store() {
                         <option value="Bags">Bags</option>
                     </select>
                 </Col>
+
             </Row>
+
+            {/* תצוגת כל המוצרים בחנות עם מעבר דינאמי לדפי המוצר */}
             <Row xs={1} sm={2} md={3} lg={4} xl={5} className="g-4 mx-auto">
                 {itemsToShow.map((item: StoreItemProps) => (
                     <Col key={item.id}>
@@ -96,6 +118,8 @@ export default function Store() {
                     </Col>
                 ))}
             </Row>
+
+            {/* פוטר */}
             <Footer />
         </>
     );
