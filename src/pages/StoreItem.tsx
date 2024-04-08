14b754fd5ef1bcb79a071469a2c96a4b74bd07a4
Gal machlev <galmachlev@gmail.com>
2024-04-08 12:26:47 +0300

    הערות בעברית storeitem.tsx

--- src/pages/StoreItem.tsx
+++ src/pages/StoreItem.tsx
@@ -1,28 +1,31 @@
 import { StoreItemProps } from '../types/storeTypes';
-//import { formatCurrency } from '../utilities/formatCurrency';
-//import { useContext } from 'react';
-//import { useParams } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { useContext, useState, useEffect } from 'react';
 import { ItemContext } from '../context/ItemContext';
 import SpecificItem from '../components/specificItem';
 
-export default function StoreItem()
- {
+//פונקציית הצגת מוצרים בדף החנות
+export default function StoreItem(){
 
+  // הפעלת הפונקציה יוז-פאראמס על מנת לקבל את המזהה הייחודי שהועבר בנתיב ה-יואראל.
   let {id } = useParams();
+
+  // שימוש באייטם-קונטקסט על מנת לקבל את מערך מוצרי החנות
   const {items} = useContext<any>(ItemContext);
 
+  // הגדרת משתנה מקומי בשם "מוצר" עם הערך ההתחלתי נאל, שיכיל את הפרטים של מוצר החנות מהמערך.
   const [product, setProduct] = useState<StoreItemProps|null>(null);
 
+  // שימוש ביוז-אקפט פעם אחת בלבד בעת טעינת הדף - מתבצע חיפוש מוצר עם אותו מזהה ייחודי כמו זה שבמערך
   useEffect(()=>{
     setProduct(items.find((item:StoreItemProps)=> item.id == Number(id)))
   }, [])
 
+  // החזרת הרכיב מוצר ספציפי עם הפרמטרים המתאימים. במידה ולא נמצא מוצר עם המזהה הייחודי המתאים יהיה ריק ולא יציג אותו
   return (
-<>
-    <SpecificItem {...product} onlyToSpecific={Boolean(product)}/>
-</>
-
+    <>
+      <SpecificItem {...product} onlyToSpecific={Boolean(product)}/>
+    </>
   )
+
 }
