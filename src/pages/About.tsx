1fba58bbcd5f8680a5ab4f986bfeac6f29ee6252
Omeremily <omeremily1@gmail.com>
2024-04-08 19:22:42 +0300

    k

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -23,19 +23,19 @@ export default function About() {
                             <h2 className="section-title">About Us</h2>
                             {/* פסקה דף אודות */}
                             <p className="section-description">
-                                Lorem ipsum d Phasellus at elit vel libero lacinia eleifend. In hac habitasse platea dictumst. Sed auctor lectus non pharetra tempus.
-                                olor sit amet, consectetur adipiscing elit. Duis eget eros vel libero rhoncus efficitur. Vestibulum auctor condimentum nisl vel dignissim. Nullam non justo ac tortor sollicitudin placerat.
+                            At SweetNSmile, we strive to deliver an exceptional shopping experience. Our platform is designed to provide a seamless interface for browsing and purchasing a wide range of products. From cutting-edge tech gadgets to stylish fashion essentials, we've curated a collection that caters to diverse tastes and preferences.                            </p>
+                            {/* פסקה דף אודות */}
+                            <p className="section-description">
+                            As aspiring developers, we are committed to leveraging the latest technologies to enhance user interactions and streamline transactions. Our goal is to not only offer quality products but also to ensure that your journey through our store is enjoyable and effortless.
+
+
                             </p>
                             {/* פסקה דף אודות */}
                             <p className="section-description">
-                                Nulla facilisPhasellus at elit vel libero lacinia eleifend. In hac habitasse platea dictumst. Sed auctor lectus non pharetra tempus.
-                                i. Mauris hendrerit velit vitae risus cursus, sed ultrices nulla ullamcorper. Ut suscipit tellus eu risus condimentum, non posuere nulla bibendum.
-                            </p>
-                            {/* פסקה דף אודות */}
-                            <p className="section-description">
-                                Phasellus at elit vel libero lacinia eleifend.Phasellus at elit vel libero lacinia eleifend. In hac habitasse platea dictumst. Sed auctor lectus non pharetra tempus.
-                                In hac habitasse platea dictumst. Sed auctor lectus non pharetra tempus.
-                            </p>
+                            Thank you for visiting SweetNSmile. Explore our offerings and feel free to reach out with any questions or feedback. We look forward to serving you!
+
+
+</p>
                         </div>
                         
                     </div>
